eab355a1dda01e959c9ed53081ada85344ca98fb
Wix55 <58708292+Wix55@users.noreply.github.com>
2025-05-26 15:20:00 +0800

    upload
    Upload

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tomas-cocacola-4axeqei0gqc-unsplash",
+      "name": "tomas-cocacola-4AxeQEi0gQc-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.29976224334158097,
+        "pitch": 0.12617552945762966,
+        "fov": 1.3645810919886565
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.29976224334158097,
+          "pitch": 0.12617552945762966,
+          "title": "Testing this link info",
+          "text": "This is a test info<div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
